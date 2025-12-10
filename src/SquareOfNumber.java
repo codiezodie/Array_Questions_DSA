14eb0dc2220846a298ce98c365c2f9c7ff11eabf
Zainab Ahmad <zainab14ahmad@gmail.com>
2025-12-11 01:33:19 +0530

    Finding square of each element in the provided array.

---
+++ src/SquareOfNumber.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class SquareOfNumber {
+    public static void main(String[] args) {
+        //Scanner sc = new Scanner(System.in);
+        for (int i = 1; i <= 10; i++) {
+            int sq = i*i;
+            System.out.println("Square of number "+ i +" is " +sq);
+        }
+    }
+}
